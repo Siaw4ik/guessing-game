5d9bc26564066a9ec69cb92c5704bd1f6ea2eeca
Slawik <peretsviach@gmail.com>
2022-08-15 10:29:36 +0200

    feat: final task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.quessNumber = Math.round((this.min + this.max)/2);
+        return this.quessNumber;
     }
 
     lower() {
-
+        this.max = this.quessNumber;
     }
 
     greater() {
-
+        this.min = this.quessNumber;
     }
 }
 
